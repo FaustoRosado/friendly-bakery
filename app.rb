36866bb6fee7fc78db8ee68eea77ad4bd91a58a8
Fausto Rosado <faustrosado@gmail.com>
2019-01-13 20:50:31 -0500

    set gets with object arrays

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require './class.rb'
+
+get '/' do
+  erb :home
+end
+
+get '/muffins' do
+  @mymuffins = [@@corn,@@banana,@@crancrunch,@@blueberry,@@cinnamon,@@dutch,@@lemon,@@coffee,@@waffle,@@chocbran,@@almond,@@blackwhite]
+  erb :muffins
+end
+
+get '/cupcakes' do
+  @mycupcakes = [@@fruitcup,@@strawchoc,@@vanillacrumb,@@mintchoc,@@redvelvet,@@cheesecake,@@confetti,@@cranorange,@@caramel,@@cookies,@@molten,@@blackforest]
+  erb :cupcakes
+end
+
+get'/cakes' do
+  @mycakes = [@@devils,@@bluecheese,@@icecream,@@walnut,@@sponge,@@pettifour,@@triplechoc,@@carrot,@@keylime,@@tiramisu,@@orange@fig]
+  erb :cakes
+end
+
+get'/contact' do
+  erb :contact
+end
