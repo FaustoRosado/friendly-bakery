80612d02f26bd0fd7a8ab92d938a0a911b3bc7bc
Fausto Rosado <faustrosado@gmail.com>
2019-01-13 19:24:55 -0500

    added class.rb and erb files

---
+++ class.rb
@@ -0,0 +1,29 @@
+class Cupcake
+  attr_accessor :name,:price,:description,:picture
+  def initialize(name,price,description,picture)
+      @name = name
+      @price = price
+      @description = description
+      @picture = picture
+  end
+end
+
+class Cakes
+  attr_accessor :name,:price,:description,:picture
+  def initialize(name,price,description,picture)
+      @name = name
+      @price = price
+      @description = description
+      @picture = picture
+  end
+end
+
+class Donut
+  attr_accessor :name,:price,:description,:picture
+  def initialize(name,price,description,picture)
+      @name = name
+      @price = price
+      @description = description
+      @picture = picture
+  end
+end
